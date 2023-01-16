9857ad811abf99d245cb483444f6e2bea6d9cd2b
dariaost19 <dariaost19@gmail.com>
2023-01-16 00:14:41 -0500

    HW8

---
+++ src/class9/ArraysDemo9.java
@@ -0,0 +1,18 @@
+package class9;
+
+public class ArraysDemo8 {
+    public static void main(String[] args) {
+
+
+        double[] arr = {10.5, 12.0, 45, 10.5, 23, 10.5};
+        int count=0;
+        //write  a program that will tell us how many times the number 10.5 is present in the array
+        for (int i = 0; i < arr.length ; i++) {
+            if (arr[i]==10.5){
+                count++; // increase the count // count=count+1;
+            }
+        }
+        System.out.println(count);
+
+    }
+}
